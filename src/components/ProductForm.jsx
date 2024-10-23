f1d57b12695954e7762d07bb3af0759a0d30793f
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-10-23 14:04:20 +0700

    answer: react-form-handling-2

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 function ProductForm() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+  const handleCreate = (event) => {
+    event.preventDefault();
+    const data = {
+      name: name,
+      price: price,
+      image: image,
+      description: description,
+    };
+    alert(JSON.stringify(data));
+  };
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleCreate}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +25,7 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(event) => setName(event.target.value)}
           />
         </label>
       </div>
@@ -22,7 +37,7 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(event) => setImage(event.target.value)}
           />
         </label>
       </div>
@@ -34,7 +49,7 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(event) => setPrice(event.target.value)}
           />
         </label>
       </div>
@@ -46,7 +61,7 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(event) => setDescription(event.target.value)}
             rows={4}
             cols={30}
           />
